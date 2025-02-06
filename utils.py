0a1a2173c3167ae777f22a534a97c9e66b2fccca
Owen Paul Ostler <oostler@uwo.ca>
2025-02-06 12:12:02 -0500

    cleaning up

--- utils.py
+++
@@ -1,7 +0,0 @@
-def get_integer_input(prompt):
-    """ Get a valid integer input from the user. """
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            print("⚠️ Invalid input! Please enter an integer.")
